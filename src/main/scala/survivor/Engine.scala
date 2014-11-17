d4fa63e277dac2ef41fa2068c40366344de0227a
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2014-11-17 18:51:57 +0100

    Change the single value Cache with a WeakMap

--- src/main/scala/survivor/Engine.scala
+++ src/main/scala/survivor/Engine.scala
@@ -8,12 +8,12 @@ class Engine(
       render: State => Unit) {
   
   private var eventsSoFar: List[Event] = List()
-  private val cache = new Cache
+  private val cache = new WeakMap
   
   def loop(time: Int): Unit = {
     // If one clock is ahead of the other we might receive inputs from the future.
     val events = eventsSoFar.dropWhile(_.time > time)
-    render(computeState(events, time))
+    render(computeState(time, events))
   }
   
   def receive(event: Event): Unit = {
@@ -22,52 +22,29 @@ class Engine(
     eventsSoFar = newer ::: event :: older
   }
   
-  def computeState(events: List[Event], time: Int): State = {
-    val state = computeState0(events, time)
-    cache.update((events, time), state)
-    state
-  }
-  
-  def computeState0(events: List[Event], time: Int): State = {
+  def computeState(time: Int, events: List[Event]): State = {
+    lazy val (nowEvents, prevEvents) = events.span(_.time == time)
+    lazy val recursively = nextState(computeState(time - 1, prevEvents), nowEvents.map(_.input))
     if(time == 0) {
       initialState
     } else {
-      cache.get((events, time)).getOrElse {
-        val (nowEvents, prevEvents) = events.span(_.time == time)
-        nextState(computeState0(prevEvents, time - 1), nowEvents.map(_.input))
-      }
+      cache.getOrElseUpdate(time, events, recursively)
     }
   }
 }
 
-class Cache {
-  var k: (List[Event], Int) = _
-  var s: State = _
+class WeakMap {
+  val size = 100
+  var array = new Array[(Int, List[Event], State)](size)
   
-  def update(key: (List[Event], Int), state: State): Unit = {
-    k = key
-    s = state
+  def getOrElseUpdate(time: Int, events: List[Event], state: => State): State = {
+    val index = time % size
+    val value = array(index)
+    if(value != null && value._1 == time && value._2 == events) {
+      value._3
+    } else {
+      array(index) = (time, events, state)
+      state
+    }
   }
-  
-  /** Optionally returns the value associated with a key. */
-  def get(key: (List[Event], Int)): Option[State] = {
-    // if(k._1.eq(key._1) && k._2 == key._2) {
-    //   System.err.println("!! HIT !! " + key._2)
-    // } else {
-    //   System.err.println("Miss, LF " + key._2 + " " + key._1)
-    //   System.err.println("Miss, Got " + k._2 + " " + k._1)
-    // }
-    if(k._1.eq(key._1) && k._2 == key._2) Some(s) else None
-  }
-  
-  /** Adds a new key/value pair to this map. */
-  // def update(key: A, value: B): Unit = ???
-  
-  /** If given key is already in this map, returns associated value. Otherwise, computes
-    * value from given expression `op`, stores with key in map and returns that value.
-  // def getOrElseUpdate(key: A, op: => B): B =
-  //   get(key) match {
-  //     case Some(v) => v
-  //     case None => val d = op; this(key) = d; d
-  //   }
 }
